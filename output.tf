f28bc0fa4d0030dc652b09ec94f0e334431529dc
MRPRAVIN-K <kumbharkarpravin7130@gmail.com>
2026-08-23 13:04:30 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,9 @@
 output "instance_ip" {
+  description = "Public IP address of the Jenkins-created EC2 instance"
+  value       = aws_instance.server.public_ip
+}
 
-value = aws_instance.jenkins_ec2.public_ip
-
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.app.repository_url
 }
