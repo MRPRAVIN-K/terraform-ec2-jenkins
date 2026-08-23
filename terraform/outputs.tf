8eed43247c26bd5f498d14dd4944a152b5ef534f
MRPRAVIN-K <kumbharkarpravin7130@gmail.com>
2026-08-24 00:05:18 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,24 @@
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.server.public_ip
+}
+
+output "public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.server.public_dns
+}
+
 output "instance_id" {
   description = "EC2 instance ID"
   value       = aws_instance.server.id
 }
 
-output "public_ip" {
-  description = "EC2 public IP"
-  value       = aws_instance.server.public_ip
-}
-
-output "public_dns" {
-  description = "EC2 public DNS"
-  value       = aws_instance.server.public_dns
-}
-
-output "private_ip" {
-  description = "EC2 private IP"
-  value       = aws_instance.server.private_ip
-}
-
 output "ami_id" {
   description = "Ubuntu AMI ID"
   value       = data.aws_ami.ubuntu.id
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.app.repository_url
 }
